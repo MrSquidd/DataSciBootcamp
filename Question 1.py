660f540181c82e68401444da29f93927f0103b98
MrSquid <YigitMBilgici1@outlook.com>
2024-09-16 01:06:37 +0300

    ilk soru çözümü

---
+++ Question 1.py
@@ -0,0 +1,5 @@
+import math
+
+print("Bu girilen iki sayının toplamını yazan bir programdır, lütfen sayıları girin.")
+sayi1, sayi2 =map(float,input("Sayıları aralarında boşlu bırakarak girin:").split())
+print(sayi1 + sayi2)
